a49ee16ced67b5bb2aaec4d4e195604e9e5e7282
ikauwsp <95624008+ikauwsp@users.noreply.github.com>
2023-11-07 02:10:59 +0800

    added effect for about me section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -2,6 +2,8 @@
 import React, {useTransition, useState} from 'react'
 import Image from 'next/image'
 import { TabButton } from './TabButton';
+import { motion } from 'framer-motion';
+
 
 const TAB_DATA = [
     {
@@ -63,7 +65,11 @@ export const AboutSection = () => {
 
   return (
    <section className='text-white'>
-    <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16'>
+    <motion.div
+        initial={{opacity:0, scale:0.5}}
+        animate={{opacity:1, scale: 1}}
+        transition={{duration:0.5}} 
+        className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16'>
         <Image src='/images/pc.jpg' width={600} height={600} style={{borderRadius: '20%'}}/>
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className='text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-teal-500 to-purple-500  mb-4'>About Me</h2>
@@ -78,7 +84,7 @@ export const AboutSection = () => {
             </div>
             <div className='mt-8'>{TAB_DATA.find((t)=>t.id === tab).content}</div>
         </div>
-    </div>
+    </motion.div>
    </section>
   )
 }
